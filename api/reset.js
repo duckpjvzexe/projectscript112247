ef309014d38885b8dc7d47e8843f5a203445a8d3
duckpjvzexe <hoangconghung398@gmail.com>
2025-09-15 12:00:21 +0700

    Update reset.js

--- api/reset.js
+++ api/reset.js
@@ -2,14 +2,15 @@ import Redis from "ioredis";
 
 const redis = new Redis(process.env.REDIS_URL, {
   password: process.env.REDIS_TOKEN,
-  tls: { rejectUnauthorized: false }
+  tls: { rejectUnauthorized: false },
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false
 });
 
 export default async function handler(req, res) {
   try {
     await redis.set("executes", 0);
     await redis.del("online_users");
-
     res.status(200).json({ message: "Reset done" });
   } catch (err) {
     res.status(500).json({ error: err.message });
