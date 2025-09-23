bc0630c543926433ea2e1ce50ff471aed4dd0ca5
duckpjvzexe <hoangconghung398@gmail.com>
2025-09-23 16:59:45 +0700

    Create auto-laylinkvip.cloud-NONE-VipMY.js

---
+++ laylinkvip.cloud/auto-laylinkvip.cloud-NONE-VipMY.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name            Auto Laylinkvip
+// @version         1.0
+// @match           https://laylinkvip.cloud/NONE/VipMY*
+// @require         https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/laylinkvip.cloud/Obfuscated.js
+// ==/UserScript==
