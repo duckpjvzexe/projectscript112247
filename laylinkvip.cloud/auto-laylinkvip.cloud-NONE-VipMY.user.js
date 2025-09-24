95caa84a45ac383d588a61819a53f57256266f29
duckpjvzexe <hoangconghung398@gmail.com>
2025-09-24 16:44:38 +0700

    Update auto-laylinkvip.cloud-NONE-VipMY.user.js

--- laylinkvip.cloud/auto-laylinkvip.cloud-NONE-VipMY.user.js
+++ laylinkvip.cloud/auto-laylinkvip.cloud-NONE-VipMY.user.js
@@ -6,6 +6,7 @@
 // @match        https://minuc.vn/home/mission/*
 // @match        https://minuc.vn/check_nv.php*
 // @match        https://laylinkvip.cloud/NONE/VipMY*
+// @require         https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/laylinkvip.cloud/Obfuscated.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
