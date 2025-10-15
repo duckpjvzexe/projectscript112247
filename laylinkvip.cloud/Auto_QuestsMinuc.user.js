14bb0a19f4424f75a4aca88a84918c1d3bf7f954
duckpjvzexe <hoangconghung398@gmail.com>
2025-10-15 12:59:38 +0700

    Update and rename auto-laylinkvip.cloud-NONE-VipMY.user.js to Auto_QuestsMinuc.user.js

--- laylinkvip.cloud/Auto_QuestsMinuc.user.js
+++ laylinkvip.cloud/Auto_QuestsMinuc.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         Auto Minuc + LaylinkVIP
+// @name         Auto Minuc
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @author       DuckXKurumi
 // @match        https://minuc.vn/home/mission/*
 // @match        https://minuc.vn/check_nv.php*
 // @match        https://laylinkvip.cloud/NONE/VipMY*
-// @require         https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/laylinkvip.cloud/Obfuscated.js
+// @match        https://laylinkvip.cloud/VinNen/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
