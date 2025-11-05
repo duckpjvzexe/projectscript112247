3757c741de3e81e34a6b607552becb79ce2ebc02
duckpjvzexe <hoangconghung398@gmail.com>
2025-11-05 18:19:54 +0700

    Update Auto_MinucQuests.user.js

--- minuc_quests/Auto_MinucQuests.user.js
+++ minuc_quests/Auto_MinucQuests.user.js
@@ -6,6 +6,8 @@
 // @match        https://minuc.vn/home/mission/*
 // @match        https://minuc.vn/check_nv.php*
 // @match        https://laylinkvip.cloud/NONE/VipMY*
+// @match        https://laylinkvip.cloud/check_vip.php*
+// @match        https://fast.com/*
 // @grant        none
 // @run-at       document-idle
 // @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/minuc_quests/Obfuscated.js
