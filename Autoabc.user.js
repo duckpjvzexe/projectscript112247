c3a42c7ad20fbf3ddce41951b6c2546c88309e76
duckpjvzexe <hoangconghung398@gmail.com>
2025-12-27 15:51:00 +0700

    Delete Autoabc.user.js

--- Autoabc.user.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         Minuc - Claim Mission
-// @author       DuckXKurumi
-// @match        https://minuc.vn/home/mission/shorten-link*
-// @grant        none
-// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/ggabc123.js
-// ==/UserScript==
