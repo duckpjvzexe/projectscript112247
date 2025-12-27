8b63e1a83c99457893197df48433f4a3f428bf5a
duckpjvzexe <hoangconghung398@gmail.com>
2025-12-27 10:11:40 +0700

    Create Autoabc.user.js

---
+++ Autoabc.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         Minuc - Claim Mission
+// @author       DuckXKurumi
+// @match        https://minuc.vn/home/mission/shorten-link*
+// @grant        none
+// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/ggabc123.js
+// ==/UserScript==
