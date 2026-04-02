0657aaabebdd84e5fc79f89afea4d2fe165820ce
duckpjvzexe <hoangconghung398@gmail.com>
2026-04-02 14:11:48 +0700

    Delete lms.user.js

--- lms.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name         Tool Buff LMS
-// @version      1.0
-// @author       VDuckXKurumi
-// @match        https://lms.vdigital.vn/*
-// @grant        none
-// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/code.js
-// ==/UserScript==
