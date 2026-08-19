9f3e88d6a2420ffe6dfb0f016778813ec87d7ed9
duckpjvzexe <hoangconghung398@gmail.com>
2026-08-19 19:12:49 +0700

    Create Auto_LaymaV2.user.js

---
+++ layma_net/Auto_LaymaV2.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Layma.net Bypass - CUTTAY PROMAX
+// @namespace    http://tampermonkey.net/
+// @version      3.67
+// @description  Auto nhiệm vụ lấy mã
+// @author       DuckXkurumi
+// @match        https://layma.net/*
+// @connect      *
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/layma_net/ObfFuncV2.js
+// ==/UserScript==
