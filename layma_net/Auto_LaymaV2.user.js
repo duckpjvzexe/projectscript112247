b30737d66ee9a5c793d46b6b9efc5444504f627e
duckpjvzexe <hoangconghung398@gmail.com>
2026-08-23 20:00:51 +0700

    Update Auto_LaymaV2.user.js

--- layma_net/Auto_LaymaV2.user.js
+++ layma_net/Auto_LaymaV2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Layma.net Bypass - CUTTAY PROMAX
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.5
 // @description  Auto nhiệm vụ lấy mã
 // @author       DuckXkurumi
 // @icon         https://files.catbox.moe/8p9l8w.jpg
@@ -10,12 +10,19 @@
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @run-at       document-end
-// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/layma_net/ObfFuncV2.js
+// @require      https://raw.githubusercontent.com/duckpjvzexe/projectscript112247/refs/heads/main/layma_net/FunctionV2.js
 // @updateURL    https://github.com/duckpjvzexe/projectscript112247/raw/refs/heads/main/layma_net/Auto_LaymaV2.user.js
 // @downloadURL  https://github.com/duckpjvzexe/projectscript112247/raw/refs/heads/main/layma_net/Auto_LaymaV2.user.js
 // ==/UserScript==
 
-var WAIT_TIME = 80; // Thời gian chờ (giây), nên để từ 80 trở lên tránh bị quét
-var AUTO_CHANGE_MISSION_ON_ERROR = true; // Tự đổi quest khi bị lỗi
+window.WAIT_TIME = 80;
+window.AUTO_CHANGE_MISSION_ON_ERROR = false;
 
-window.LayMaScript();
+if (typeof window.LayMaScript === 'function') {
+    window.LayMaScript({
+        'gmXhr': typeof GM_xmlhttpRequest !== 'undefined' ? GM_xmlhttpRequest : null,
+        'uWindow': typeof unsafeWindow !== 'undefined' ? unsafeWindow : null
+    });
+} else {
+    console.error("Không tìm thấy thư viện. Vui lòng kiểm tra lại link @require!");
+}
