35547929f155575235633dcd759b57246a841202
duckpjvzexe <hoangconghung398@gmail.com>
2026-08-26 15:56:56 +0700

    Update Auto_LaymaV2.user.js

--- layma_net/Auto_LaymaV2.user.js
+++ layma_net/Auto_LaymaV2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Layma.net Bypass - CUTTAY PROMAX
 // @namespace    http://tampermonkey.net/
-// @version      1.0.6
+// @version      1.0.7
 // @description  Auto nhiệm vụ lấy mã
 // @author       DuckXkurumi
 // @icon         https://files.catbox.moe/8p9l8w.jpg
@@ -17,12 +17,3 @@
 
 window.WAIT_TIME = 80;
 window.AUTO_CHANGE_MISSION_ON_ERROR = false;
-
-if (typeof window.LayMaScript === 'function') {
-    window.LayMaScript({
-        'gmXhr': typeof GM_xmlhttpRequest !== 'undefined' ? GM_xmlhttpRequest : null,
-        'uWindow': typeof unsafeWindow !== 'undefined' ? unsafeWindow : null
-    });
-} else {
-    console.error("Không tìm thấy thư viện. Vui lòng kiểm tra lại link @require!");
-}
